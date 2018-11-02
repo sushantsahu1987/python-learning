0a8fa2a1e964c376b7f60aaa15cfa9c57a0570d1
Sushant Sahu <sushant87@gmail.com>
2018-11-03 00:44:01 +0530

    basic tutorial 1

---
+++ basics_1.py
@@ -0,0 +1,55 @@
+print("hello world")
+
+if 5 > 2:
+    print("this statement is true")
+
+x = 10
+y = "abcd"
+
+print(x)
+print(y)
+
+w = 20
+
+print("%d this expression is example of combining a number with an expression" % w)
+
+print("numeric types")
+a = 1
+b = 2.8
+c = 1j
+print(type(a))
+print(type(b))
+print(type(c))
+
+print("casting int, float, str")
+print(int(1))
+print(int(2.2))
+print(int("3"))
+
+print(float(1))
+print(float(2.2))
+print(float("3"))
+
+print(str(1))
+print(str(2.2))
+print(str("3"))
+
+
+print("string literals")
+hello = "hello world"
+print(hello[0])
+print(hello[0:5])
+
+hello = " hello world"
+print(hello.strip())
+print(len(hello))
+
+hello = "HELLO world"
+print(hello.lower())
+print(hello.upper())
+print(hello.replace("H","r"))
+
+hello = "trillo:kello"
+print(hello.split(":"))
+
+
