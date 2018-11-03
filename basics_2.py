a4a3853d795bcc884031e4c79aca708e78a34434
Sushant Sahu <sushant87@gmail.com>
2018-11-03 11:48:39 +0530

    code update

---
+++ basics_2.py
@@ -0,0 +1,21 @@
+
+# Assignment operators
+x = 5
+print(x)
+x/=5
+print("x = %d"%x)
+y = 10
+
+# Floor division
+print(y//3)
+print(y/3)
+print(y&3)
+
+# Exponential
+print(y**3)
+
+#Zero fill left shift	
+print(y<<3)
+
+#Signed right shift	
+print(y>>3)
